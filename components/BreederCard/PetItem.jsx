d033c74ef958aee20d6d60d43ee07482b68d0a0f
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-13 21:36:41 -0400

    revert

--- components/BreederCard/PetItem.jsx
+++ components/BreederCard/PetItem.jsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
+import Link from 'next/link';
 const PetItem = ({ pet }) => {
   return (
-    <Link href="/pet/[id]" as={`/pet/${pet.id}`}>
-      <div className="bg-yellow-600 rounded-md hover:cursor-pointer shadow-lg">
-        <div className="bg-blue-500 p-8 rounded-md">Picture</div>
-        <div className="p-5">
+    <Link href='/pet/[id]' as={`/pet/${pet.id}`}>
+      <div className='bg-yellow-600 rounded-md hover:cursor-pointer shadow-lg'>
+        <div className='bg-blue-500 p-8 rounded-md'>Picture</div>
+        <div className='p-5'>
           <h3>{pet.name}</h3>
           <p>{pet.description}</p>
           <p>{pet.dateOfBirth}</p>
@@ -15,5 +15,4 @@ const PetItem = ({ pet }) => {
     </Link>
   );
 };
-
 export default PetItem;
