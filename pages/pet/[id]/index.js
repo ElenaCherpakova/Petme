f289daa7fa01878fd3861afec88f349fa6cec1e0
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-14 00:26:27 -0400

    fix all conflicts

--- pages/pet/[id]/index.js
+++ pages/pet/[id]/index.js
@@ -1,21 +1,22 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 const pet = ({ pet }) => {
   return (
-    <div className="bg-yellow-600 rounded-md hover:cursor-pointer shadow-lg container mx-auto my-4">
-      <div className="bg-blue-500 p-8 rounded-md">Picture</div>
-      <div className="p-5">
+    <div className='bg-yellow-600 rounded-md hover:cursor-pointer shadow-lg container mx-auto my-4'>
+      <div className='bg-blue-500 p-8 rounded-md'>Picture</div>
+      <div className='p-5'>
         <h3>{pet.name}</h3>
         <p>{pet.description}</p>
         <p>{pet.dateOfBirth}</p>
         <p>{pet.gender}</p>
         <p>{pet.price}</p>
       </div>
-
-      <div className="flex justify-center bg-slate-300 w-1/2 mx-auto p-6 rounded-md">
-        <Link href="/">Go Back</Link>
-      </div>
+      <Link href='/'>
+        <div className='flex justify-center bg-slate-300 w-1/2 mx-auto p-6 rounded-md'>
+          Go Back
+        </div>
+      </Link>
     </div>
   );
 };
@@ -24,7 +25,7 @@ export default pet;
 
 export const getServerSideProps = async (context) => {
   const res = await fetch(
-    `http://localhost:3000/api/Pets/${context.params.id}`
+    `http://localhost:3000/api/Pets/${context.params.id}`,
   );
   const pet = await res.json(res);
 
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,23 +2,100 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const customer = await prisma.customer.create({
+  const breeder = await prisma.Breeder.createMany({
+    data: [
+      {
+        firstName: 'Bob',
+        lastName: 'Fork',
+        email: 'Bobfork@hotmail.com',
+        password: 'password123',
+        phoneNumber: '123-123-123',
+        address: '2250 farmville rd',
+        profileImage: 'test',
+      },
+      {
+        firstName: 'Mark',
+        lastName: 'Korn',
+        email: 'Mark@hotmail.com',
+        password: 'password123',
+        phoneNumber: '123-123-123',
+        address: '2230 farmville rd',
+        profileImage: 'test',
+      },
+    ],
+  });
+
+  const pet = await prisma.Pet.createMany({
+    data: [
+      {
+        name: 'Alex',
+        dateOfBirth: 'November 23, 2019',
+        description: 'Energetic',
+        profileImage: 'test123',
+        price: 1000,
+        gender: 'Male',
+        breederId: 1,
+      },
+      {
+        name: 'Coconut',
+        dateOfBirth: 'November 22, 2019',
+        description: 'Energetic',
+        profileImage: 'test123',
+        price: 1000,
+        gender: 'Male',
+        breederId: 2,
+      },
+    ],
+  });
+
+  const customer = await prisma.Customer.create({
     data: {
       firstName: 'John',
-      lastName: 'Doe',
-      email: 'john@test.com',
-      password: 'test',
-      phoneNumber: '1234',
+      lastName: 'Artuz',
+      email: 'johnartuz@hotmail.com',
+      password: 'password123',
+      phoneNumber: '6475429109',
       address: 'test',
-      profileImage: 'yes',
+      profileImage: 'test',
     },
   });
-  console.log(customer);
+
+  const petCategory = await prisma.PetCategory.createMany({
+    data: [
+      {
+        pureBreed: false,
+        breedName: 'Poodle, shitzu',
+        categoryName: 'Dog',
+        petId: 1,
+      },
+      {
+        pureBreed: true,
+        breedName: 'Poodle',
+        categoryName: 'Dog',
+        petId: 2,
+      },
+    ],
+  });
+
+  const sale = await prisma.Sale.createMany({
+    data: [
+      {
+        salesDate: 'April 12, 2022',
+        breederId: 1,
+        customerId: 1,
+        petId: 1,
+        paymentStatus: 'Paid',
+        amountPayed: 1000,
+        refNumber: 123123,
+      },
+    ],
+  });
 }
 
 main()
   .catch((e) => {
-    console.error(e.message);
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
