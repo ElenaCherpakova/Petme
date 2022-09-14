11d5a5d080cc4c4c366bd183fac4b82e40f26d0a
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-13 23:01:00 -0400

    revert

--- pages/api/Pets/[id].js
+++ pages/api/Pets/[id].js
@@ -1,4 +1,4 @@
-import { prisma } from "../../../lib/prisma";
+import { prisma } from '../../../lib/prisma';
 
 export default async function handler({ query: { id } }, res) {
   const filtered = await prisma.Pet.findUnique({
